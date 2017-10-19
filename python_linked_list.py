ab6de0ce874d05db7644f0b1dbcd93983f2fd9aa
Amitesh Pathak <amitesh1095@gmail.com>
2017-10-19 13:07:27 +0530

    Added Unordered Linked List in python

---
+++ python_linked_list.py
@@ -0,0 +1,107 @@
+#We are going to create an unordered linked list in this program using python
+
+
+#To kick off the process first we need to create a "node" class which, obviously, represents our node.
+
+# The Node class is self suffiecient to perform any operations we might wish to perform on any node in the list.
+
+class Node:
+    def __init__(self,initdata):
+        self.data = initdata
+        self.next = None
+
+    def getData(self):
+        return self.data
+
+    def getNext(self):
+        return self.next
+
+    def setData(self,newdata):
+        self.data = newdata
+
+    def setNext(self,newnext):
+        self.next = newnext
+
+
+class UnorderedList:
+
+    def __init__(self):
+        self.head = None
+   
+    #check if empty
+    def isEmpty(self):
+        return self.head == None
+    
+    #add a node
+    def add(self,item):
+        temp = Node(item)
+        temp.setNext(self.head)
+        self.head = temp
+
+    #size of list
+    def size(self):
+        current = self.head
+        count = 0
+        while current != None:
+            count = count + 1
+            current = current.getNext()
+
+        return count
+    
+    #search for a node
+    def search(self,item):
+        current = self.head
+        found = False
+        while current != None and not found:
+            if current.getData() == item:
+                found = True
+            else:
+                current = current.getNext()
+
+        return found
+
+    #remove a node
+    def remove(self,item):
+        current = self.head
+        previous = None
+        found = False
+        while not found:
+            if current.getData() == item:
+                found = True
+            else:
+                previous = current
+                current = current.getNext()
+
+        if previous == None:
+            self.head = current.getNext()
+        else:
+            previous.setNext(current.getNext())
+
+#Initialize the list//create an object
+mylist = UnorderedList()
+
+#Next, we perform some operations on our linked list.
+
+mylist.add(31)
+mylist.add(77)
+mylist.add(17)
+mylist.add(93)
+mylist.add(26)
+mylist.add(54)
+
+print(mylist.size())
+print(mylist.search(93))
+print(mylist.search(100))
+
+mylist.add(100)
+print(mylist.search(100))
+print(mylist.size())
+
+mylist.remove(54)
+print(mylist.size())
+mylist.remove(93)
+print(mylist.size())
+mylist.remove(31)
+print(mylist.size())
+print(mylist.search(93))
+
